771bb5e65bda4f98053896dd48ef0469b2b68c5e
TheRjDev <ramsus85@protonmail.com>
2022-08-04 16:14:13 -0500

    added second version of the script

--- name_decorator.py
+++ name_decorator.py
@@ -4,46 +4,8 @@ from typing import Counter
 from numpy import inner
 import pandas as pd
 
-def decorator(df, words):
-    def wrapper(func):
-        first_names = df['first_name'].tolist()
-        last_names = df['last_name'].tolist()
-        links = df['link'].tolist()
-        new_file = ''
-        def inner():
-            
-            for i in len(words):
-                if words[i] in first_names:
-                    name_index = first_names.index(words[i])
-                    try:
-                        if words[i + 1] in last_names:
-                            new_file += f'<img src="{links[name_index]}" alt="{words[i]} {words[i + 1]}"> {words[i]} {words[i + 1]} '
-                        elif words[i - 1] in last_names:
-                            break
-                        else:
-                            new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} {words[i]} '
-                    except IndexError:
-                        new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} '
 
-                elif words[i] in last_names:
-                    name_index = last_names.index(words[i])
-                    try:
-                        if words[i - 1] in first_names:
-                            break
-                        elif words[i + 1] in first_names:
-                            new_file += f'<img src="{links[name_index]}" alt="{words[i]} {words[i + 1]}"> {words[i]} {words[i + 1]} '
-                    except IndexError:
-                        new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} '
-                else:
-                    new_file += words[i] + ' '
-
-        with open('new_file.md', mode='w', encoding='utf-8') as f:
-            f.write(new_file)
-        return inner
-    return wrapper
-
-
-def main():
+def decorator(func):
     df = pd.read_csv('names.csv')
     file = None
 
@@ -53,6 +15,46 @@ def main():
     file.replace('\n', ' ')
 
     words = file.split(' ')
+    first_names = df['first_name'].tolist()
+    last_names = df['last_name'].tolist()
+    links = df['link'].tolist()
+
+    def wrapper():
+        new_file = ''
+
+        for i in range(len(words)):
+            if words[i] in first_names:
+                name_index = first_names.index(words[i])
+                try:
+                    if words[i + 1] in last_names:
+                        new_file += f'<img src="{links[name_index]}" alt="{words[i]} {words[i + 1]}"> {words[i]} {words[i + 1]} '
+                    elif words[i - 1] in last_names:
+                        break
+                    else:
+                        new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} {words[i]} '
+                except IndexError:
+                    new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} '
+
+            elif words[i] in last_names:
+                name_index = last_names.index(words[i])
+                try:
+                    if words[i - 1] in first_names:
+                        break
+                    elif words[i + 1] in first_names:
+                        new_file += f'<img src="{links[name_index]}" alt="{words[i]} {words[i + 1]}"> {words[i]} {words[i + 1]} '
+                except IndexError:
+                    new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} '
+            else:
+                new_file += words[i] + ' '
+
+        with open('new_file.md', mode='w', encoding='utf-8') as f:
+            f.write(new_file)
+        func()
+    return wrapper
+
+@decorator
+def main():
+    print('works')
 
 
 if __name__ == '__main__':
---
+++ new_file.md
@@ -0,0 +1,1 @@
+<img src="https://static.wikia.nocookie.net/virtualyoutuber/images/7/7f/Sakura_Miko_2020_Portrait.png/revision/latest/scale-to-width-down/1000?cb=20210802134259" alt="Sakura Miko"> Sakura Miko 
