69c8b1fcc133940eda025aac13af245cc2f63663
TheRjDev <ramsus85@protonmail.com>
2022-08-04 16:00:32 -0500

    added initial version of the name_decorator

--- name_decorator.py
+++ name_decorator.py
@@ -1,31 +1,59 @@
+from curses import wrapper
 from os import link
 from typing import Counter
+from numpy import inner
 import pandas as pd
 
-def decorator():
+def decorator(df, words):
     def wrapper(func):
-        pass
+        first_names = df['first_name'].tolist()
+        last_names = df['last_name'].tolist()
+        links = df['link'].tolist()
+        new_file = ''
+        def inner():
+            
+            for i in len(words):
+                if words[i] in first_names:
+                    name_index = first_names.index(words[i])
+                    try:
+                        if words[i + 1] in last_names:
+                            new_file += f'<img src="{links[name_index]}" alt="{words[i]} {words[i + 1]}"> {words[i]} {words[i + 1]} '
+                        elif words[i - 1] in last_names:
+                            break
+                        else:
+                            new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} {words[i]} '
+                    except IndexError:
+                        new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} '
+
+                elif words[i] in last_names:
+                    name_index = last_names.index(words[i])
+                    try:
+                        if words[i - 1] in first_names:
+                            break
+                        elif words[i + 1] in first_names:
+                            new_file += f'<img src="{links[name_index]}" alt="{words[i]} {words[i + 1]}"> {words[i]} {words[i + 1]} '
+                    except IndexError:
+                        new_file += f'<img src="{links[name_index]}" alt="{words[i]}"> {words[i]} '
+                else:
+                    new_file += words[i] + ' '
+
+        with open('new_file.md', mode='w', encoding='utf-8') as f:
+            f.write(new_file)
+        return inner
     return wrapper
 
+
 def main():
-    df=pd.read_csv('names.csv')
-    file=None
+    df = pd.read_csv('names.csv')
+    file = None
+
     with open('test.md', mode='r', encoding='utf-8') as f:
         file = f.read()
-    
-    first_names = df['first_name'].tolist()
-    last_names = df['last_name'].tolist()
-    links = df['link'].tolist()
-    new_file=''
 
-    for words in file:
-        if words in first_names:
-            index = first_names.index(words)
-            try:
-                if words[index+1] == last_names[index+1]:
+    file.replace('\n', ' ')
 
-            
-            new_file += f'<img src="{links[index]}" alt="{words}">'
+    words = file.split(' ')
+
 
 if __name__ == '__main__':
     main()
